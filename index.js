d8bca9404f83480ef28cd482a121fb7b6d6d5ea9
gaeun58 <gaeun58@naver.com>
2021-09-13 13:21:52 +0900

    practice js

---
+++ index.js
@@ -0,0 +1,34 @@
+console.log('Hello World!')
+
+// 변수 선언
+let espressoPrice = 3000;
+let lattePrice = 4000;
+
+console.log(espressoPrice * 3);
+console.log(espressoPrice * 2 + lattePrice * 3);
+
+// 함수 선언
+function greetings() {
+    console.log('Hi');
+    console.log('안녕');
+    console.log('Guten Tag');
+    console.log('Bonjour');
+};
+// 함수 호출
+greetings();
+
+// 함수 선언 실습: 애국가 가사
+function printChorus() {
+    console.log('무궁화 삼천리 화려 강산');
+    console.log('대한 사람 대한으로 길이 보전하세');
+}
+
+console.log('1절');
+console.log('동해 물과 백두산이 마르고 닳도록');
+console.log('하느님이 보우하사 우리나라 만세');
+printChorus();
+
+console.log('2절');
+console.log('남산 위에 저 소나무 철갑을 두른 듯');
+console.log('바람서리 불변함은 우리 기상일세');
+printChorus();
