43e08a05f12f1aa6329c1b71d10b7d7a2b8aa780
gaeun58 <gaeun58@naver.com>
2021-09-23 20:48:26 +0900

    버튼 클릭 시 영화 정보 제공

---
+++ mogakco/day9.js
@@ -0,0 +1,34 @@
+let frozen = {
+    movieTitle: "Frozen1",
+    openingDay: "2014.01.16",
+    genre: "Animation",
+    cast: "Kristen Bell, Idina Menzel"
+};
+
+let aladdin = {
+    movieTitle: "Aladdin",
+    openingDay: "2019.05.23.",
+    genre: "Adventure",
+    cast: "Will Smith, Mena Massoud, Naomi Scott"
+};
+
+let lalaland = {
+    movieTitle: "La La land",
+    openingDay: "2016.12.07.",
+    genre: "Drama",
+    cast: "Ryan Gosling, Emma Stone"
+};
+
+let movieTitle = document.getElementsByClassName("movieTitle");
+let info = document.getElementsByClassName("info");
+let openingDay = document.getElementsByClassName("openingDay");
+let genre = document.getElementsByClassName("genre");
+let cast = document.getElementsByClassName("cast");
+
+function showMovieInfo(title) {
+    movieTitle[0].innerHTML = title.movieTitle;
+    openingDay[0].innerHTML = title.openingDay;
+    genre[0].innerHTML = title.genre;
+    cast[0].innerHTML = title.cast;
+    info[0].classList.remove("hide")
+}
