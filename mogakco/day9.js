4453855d490519c618ee602d4e49c3ee668544a4
gaeun58 <gaeun58@naver.com>
2021-09-24 16:41:31 +0900

    포스터이미지 추가

--- mogakco/day9.js
+++ mogakco/day9.js
@@ -2,21 +2,24 @@ let frozen = {
     movieTitle: "Frozen1",
     openingDay: "2014.01.16",
     genre: "Animation",
-    cast: "Kristen Bell, Idina Menzel"
+    cast: "Kristen Bell, Idina Menzel",
+    src: "frozen1.jpg"
 };
 
 let aladdin = {
     movieTitle: "Aladdin",
     openingDay: "2019.05.23.",
     genre: "Adventure",
-    cast: "Will Smith, Mena Massoud, Naomi Scott"
+    cast: "Will Smith, Mena Massoud, Naomi Scott",
+    src: "aladdin.jpg"
 };
 
 let lalaland = {
     movieTitle: "La La land",
     openingDay: "2016.12.07.",
     genre: "Drama",
-    cast: "Ryan Gosling, Emma Stone"
+    cast: "Ryan Gosling, Emma Stone",
+    src: "lalaland.jpg"
 };
 
 let movieTitle = document.getElementsByClassName("movieTitle");
@@ -30,5 +33,6 @@ function showMovieInfo(title) {
     openingDay[0].innerHTML = title.openingDay;
     genre[0].innerHTML = title.genre;
     cast[0].innerHTML = title.cast;
-    info[0].classList.remove("hide")
+    info[0].classList.remove("hide");
+    document.getElementById("img").src = title.src;
 }
