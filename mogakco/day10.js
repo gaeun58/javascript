e8ac034c4c78e66d1c52389694be5351b8f269e3
gaeun58 <gaeun58@naver.com>
2021-09-27 17:14:38 +0900

    전화걸기 버튼을 누르면 2초 뒤에 이름과 전화번호 출력하기2

--- mogakco/day10.js
+++ mogakco/day10.js
@@ -7,3 +7,13 @@ function call() {
     nameBox.innerText = '김지석';
     callBtn.innerText = '010-1234-5678';
 }
+
+/*
+function call(){
+    setTimeout(function(){
+        document.getElementsByClassName('call')[0].style.display = 'none';
+        document.getElementsByClassName('name')[0].style.display = 'flex';
+        document.getElementsByClassName('number')[0].style.display = 'flex';
+    },2000);
+}
+*/
